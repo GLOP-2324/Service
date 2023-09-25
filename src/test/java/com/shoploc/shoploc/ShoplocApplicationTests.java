dcb5cd57834eaf222e4d72dbbb04cc1607e3fcc0
El Hajj Joe <joe.elhajj.etu@b12p19.fil.univ-lille.fr>
2023-09-25 09:07:16 +0200

    First commit

---
+++ src/test/java/com/shoploc/shoploc/ShoplocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoploc.shoploc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
