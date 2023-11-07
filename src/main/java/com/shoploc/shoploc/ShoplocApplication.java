bb3dd511fb34f57ff106a7e26201fd539ba8b069
Joe El hajj <joe.elhajj.etu@univ-lille.fr>
2023-11-07 15:17:53 +0100

    test ci/cd

--- src/main/java/com/shoploc/shoploc/ShoplocApplication.java
+++ src/main/java/com/shoploc/shoploc/ShoplocApplication.java
@@ -1,12 +1,8 @@
 package com.shoploc.shoploc;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ShoplocApplication {
