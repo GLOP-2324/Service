d583c34b94670ede848439dadbc6ee99be250ad4
Nadine <nadinesaadi42@yahoo.fr>
2023-12-12 15:32:39 +0100

    patch sécurisation avec Token

--- src/main/java/com/shoploc/shoploc/config/HeaderInterceptor.java
+++ src/main/java/com/shoploc/shoploc/config/HeaderInterceptor.java
@@ -7,14 +7,12 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
 public class HeaderInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         String token = request.getHeader("Authorization");
-        System.out.println("Authorization Header: " + token);
 
         if (token != null && token.startsWith("Bearer ")) {
             // Token présent, autorisez la requête à continuer vers les méthodes du contrôleur
