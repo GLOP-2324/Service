6fe95dee8c023110d71ee98add780836a054b283
JOEELHAJJ <joeelhajj53@gmail.com>
2024-04-06 02:22:57 +0200

    unit test

--- src/main/java/com/shoploc/shoploc/domain/type/TypeProductServiceImpl.java
+++ src/main/java/com/shoploc/shoploc/domain/type/TypeProductServiceImpl.java
@@ -1,8 +1,10 @@
 package com.shoploc.shoploc.domain.type;
 
+import com.shoploc.shoploc.domain.product.Product;
 import com.shoploc.shoploc.domain.product.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,14 +21,13 @@ public class TypeProductServiceImpl implements TypeProductService {
         return typeProductRepository.findAll();
     }
 
-    @Override
+    /*@Override
     public TypeProduct createType(String libelle) {
         TypeProduct typeProduct = new TypeProduct();
         typeProduct.setLibelle(libelle);
-
         return typeProductRepository.save(typeProduct);
 
-    }
+    }*/
 
     @Override
     public TypeProduct getById(Integer id) {
