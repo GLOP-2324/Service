0cff5948e29fb877e3a1cbf8809755d343c1dbdc
JOEElHAJJ <113100387+JOEElHAJJ@users.noreply.github.com>
2024-04-08 02:59:04 +0200

    Update CorsConfig.java

--- src/main/java/com/shoploc/shoploc/config/CorsConfig.java
+++ src/main/java/com/shoploc/shoploc/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://172.28.101.32");
+        config.addAllowedOrigin("*");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         source.registerCorsConfiguration("/**", config);
